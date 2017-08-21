57cc3d8896c1ddea1a9db2f30d6ccf0366b57027
Zian <zauvohi@gmail>
2017-08-20 21:22:23 -0500

    Removed comment

--- app/channels/comments_channel.rb
+++ app/channels/comments_channel.rb
@@ -1,6 +1,5 @@
 class CommentsChannel < ApplicationCable::Channel
   def subscribe(data)
-    #puts "POST ID: #{data['post_id']}"
     post_id = data['post_id'].to_i
     @post = Post.find(post_id)
     stream_for @post
