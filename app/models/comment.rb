cd4ffaf8a8b2d2f21d8e0d59f881bcb45dd838aa
Zian <zauvohi@gmail>
2017-08-21 11:05:52 -0500

    Added validations to models. Fixed views for comments and posts that got messed up after downgrading bootstrap

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :post
+  validates :body, presence: true
   after_create_commit { CommentBroadcastJob.perform_later self, 'created' }
   after_destroy { CommentBroadcastJob.perform_later self, 'destroyed' }
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ApplicationRecord
   has_many :comments, dependent: :delete_all
+  validates :content, :title, presence: true
 end
