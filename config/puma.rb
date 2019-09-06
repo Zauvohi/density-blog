1aa085bdc91adb34816e35ef45bbeb39f7f118b9
Zian Aguirre <zauvohi@gmail.com>
2019-09-06 11:02:15 -0500

    updates

--- config/puma.rb
+++ config/puma.rb
@@ -1,10 +1,10 @@
 # Change to match your CPU core count
-workers 2
+workers 1
 
 # Min and Max threads per worker
 threads 1, 6
 
-app_dir = File.expand_path("../", __FILE__)
+app_dir = File.expand_path("../..", __FILE__)
 shared_dir = "#{app_dir}/shared"
 
 # Default to production
