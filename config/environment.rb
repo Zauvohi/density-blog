dab08ffb5558f681cb819284914902a10521e7a0
Zian Aguirre <zauvohi@gmail.com>
2019-09-05 19:39:56 -0500

    updated to environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
 # Load the Rails application.
 require_relative 'application'
 
+Rails.logger = Logger.new(STDOUT)
+config.logger = ActiveSupport::Logger.new("shared/logs/#{Rails.env}.log")
 # Initialize the Rails application.
 Rails.application.initialize!
