aedd36d2c2c2b3602fa633c7464ee33137f0375d
Zian Aguirre <zauvohi@gmail.com>
2019-09-05 19:41:23 -0500

    reverting change

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,5 @@
 # Load the Rails application.
 require_relative 'application'
 
-Rails.logger = Logger.new(STDOUT)
-config.logger = ActiveSupport::Logger.new("shared/logs/#{Rails.env}.log")
 # Initialize the Rails application.
 Rails.application.initialize!
